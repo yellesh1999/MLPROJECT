8db68b35caefa39446f2c1c7c6392b54f23269a9
D yellesh <yelleshdeshmukh26@gmail.com>
2025-06-02 02:10:56 +0530

    changes

--- application.py
+++ application.py
@@ -39,6 +39,9 @@ def predict_datapoint():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+    
 
         
