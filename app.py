30a839e159c57782e23a3900351a9414fa0e17a2
D yellesh <yelleshdeshmukh26@gmail.com>
2025-06-02 14:06:02 +0530

    changes

--- app.py
+++ app.py
@@ -39,9 +39,13 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
     
-    
+import os
+  
 
-if __name__=="__main__":      
-    app.run(host="0.0.0.0",port=80) 
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
         
