d2ce682ecc0ec21e00e7df5a1bf13a3e59bf854a
D yellesh <yelleshdeshmukh26@gmail.com>
2025-06-02 13:24:17 +0530

    some app changes

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+    
+
+
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
@@ -9,11 +12,11 @@ application=Flask(__name__)
 
 app=application
 
-##route for a homepage
+## Route for a home page
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
@@ -26,8 +29,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score')))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score')))
         
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
@@ -36,12 +39,9 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
     
-
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
     
+
+if __name__=="__main__":      
+    app.run(host="0.0.0.0",port=80) 
 
         
